4dd7112b08935f78c9542022b22457351771536e
Basma elkhayat <b.khayat@myzeal.app>
2022-01-30 14:57:03 +0200

    add new component to test lazy loading in child component

--- src/app/lazy-loading/lazy-loading-routing.module.ts
+++ src/app/lazy-loading/lazy-loading-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LazyLoadingComponent } from './lazy-loading.component';
 
-const routes: Routes = [{ path: '', component: LazyLoadingComponent }];
+const routes: Routes = [
+    { path: '', component: LazyLoadingComponent },
+    {
+        path: 'child',
+        loadChildren: () => import('./child/child.module').then((m) => m.ChildModule),
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class LazyLoadingRoutingModule {}
--- src/app/lazy-loading/lazy-loading.module.ts
+++ src/app/lazy-loading/lazy-loading.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LazyLoadingRoutingModule } from './lazy-loading-routing.module';
 import { LazyLoadingComponent } from './lazy-loading.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [LazyLoadingComponent],
-  imports: [CommonModule, LazyLoadingRoutingModule],
+    declarations: [LazyLoadingComponent],
+    imports: [LazyLoadingRoutingModule, CommonModule, FormsModule, HttpClientModule],
 })
 export class LazyLoadingModule {}
